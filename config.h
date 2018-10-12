c89d5be8bb39d10fd3a3bedbbec3469daa204dc2
Paul <ping.paul@hotmail.com>
2018-10-12 16:33:11 +0200

    added rules for runorraise applications

--- config.h
+++ config.h
@@ -29,8 +29,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1,            0,           -1 },
+	{ "Gimp",     NULL,       NULL,       0<<0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1<<0,            0,           -1 },
+	{ "mutt",     NULL,       NULL,       1<<4,            0,           -1 },
+	{ "calcurse", NULL,       NULL,       1<<4,            0,           -1 },
 };
 
 /* layout(s) */
