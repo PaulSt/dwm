ac0fd8ca766d2b77f62030eaca28d0fb0021ad5a
Paul <ping.paul@hotmail.com>
2018-11-24 16:56:41 +0100

    added different color scheme

--- config.h
+++ config.h
@@ -14,10 +14,13 @@ static const char col_gray2[]       = "#282828";
 static const char col_gray3[]       = "#928374";
 static const char col_gray4[]       = "#fbf1c7";
 static const char col_cyan[]        = "#458588";
+static const char col_gb_bg[]       = "#282828";
+static const char col_gb_aqua[]     = "#8ec07c";
+static const char col_gb_fg[]       = "#ebdbb2";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gb_fg, col_gb_bg, col_gb_bg},
+	[SchemeSel]  = { col_gb_aqua, col_gb_bg,  col_gb_aqua},
 };
 
 /* tagging */
